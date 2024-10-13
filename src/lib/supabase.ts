00fcb42e46bb2aa5418034ceb50549c5619d3c3c
Ritik6559 <ritikjoshi741@gmail.com>
2024-10-14 00:17:57 +0530

    initializing supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://sxiniehpxhopqjpogeub.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4aW5pZWhweGhvcHFqcG9nZXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg4NDUwMjcsImV4cCI6MjA0NDQyMTAyN30.ZpeD6p8vGOYklYMv4pkJs08oMmAVD4mKLSLbMs_19jM';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
